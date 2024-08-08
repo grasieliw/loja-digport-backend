bf9828ced4cf2b2992a0f17769c5f16567b74198
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2024-08-08 13:53:50 -0300

    routes+controller

--- server.go
+++ server.go
@@ -1,52 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-
-	"github.com/grasieliw/loja-digport-backend/blob/main/main.go/model"
+	"github.com/grasieliw/loja-digport-backend/blob/routes"
 )
 
 func StarServer() {
 
-	http.HandleFunc("/produtos", produtosHandler)
-	http.ListenAndServe(":8080", nil)
-}
+	routes.HandleRequests()
 
-func produtosHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == "GET" {
-		getProdutos(w, r)
-	} else if r.Method == "POST" {
-		addProdutos(w, r)
-	}
-
-}
-
-func getProdutos(w http.ResponseWriter, r *http.Request) {
-	queryParam := r.URL.Query().Get("nome")
-
-	if queryParam != "" {
-		prodBusca := buscaPorNome(queryParam)
-		json.NewEncoder(w).Encode(prodBusca)
-	} else {
-		produtos := catalogo()
-		json.NewEncoder(w).Encode(produtos)
-	}
-}
-
-func addProdutos(w http.ResponseWriter, r *http.Request) {
-	var produto model.Produto
-	json.NewDecoder(r.Body).Decode(&produto)
-
-	registrarProdutos(produto)
-
-	jsonData, err := json.Marshal(produto)
-
-	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	w.Write(jsonData)
 }
