8ab1d35ebd6e82c723b2d476c9ca63e34fced3a9
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2024-09-05 18:04:48 -0300

    updates DB

--- db/db.go
+++ db/db.go
@@ -9,6 +9,11 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func InitDB() {
+
+	CriaTabelas()
+}
+
 func ConectaBancoDados() *sql.DB {
 	dbPass := os.Getenv("DB_PASS")
 	//connStr := "user=postgres dbname=digport_loja password=digport host=localhost sslmode=disable"
@@ -18,4 +23,46 @@ func ConectaBancoDados() *sql.DB {
 		log.Fatal(err)
 	}
 	return db
+}
+
+func CriaTabelas() {
+	db := ConectaBancoDados()
+	defer db.Close()
+
+	createUsersTable := `
+CREATE TABLE IF NOT EXISTS USUARIO (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR,
+	telefone VARCHAR,
+	endereco VARCHAR,
+	email VARCHAR NOT NULL UNIQUE,
+	senha VARCHAR NOT NULL
+);`
+	_, err := db.Exec(createUsersTable)
+
+	if err != nil {
+		panic("Erro ao criar tabela usuario")
+	} else {
+		fmt.Println("Tabela usuario criada")
+	}
+
+	createProductsTableScript := `
+    CREATE TABLE IF NOT EXISTS produtos
+    (
+    id varchar primary key,
+    nome varchar,
+    preco  float8,
+    descricao varchar,
+    imagem varchar,
+    quantidade int
+    );`
+
+	_, err = db.Exec(createProductsTableScript)
+
+	if err != nil {
+		panic("Erro ao criar tabela usuario")
+	} else {
+		fmt.Println("Tabela usuario criada")
+	}
+
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/grasieliw/loja-digport-backend/db"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	db.InitDB()
 	StarServer()
 
 	//fmt.Println("Bem Vindo(a) à loja DigPort")
